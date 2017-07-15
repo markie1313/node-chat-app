ab2615b5e62fa67fb62ef2c16686e31150284734
markie1313 <markie13@outlook.com>
2017-07-15 08:12:20 -0500

    Finally fixed the bug that caused the "welcome to the chat" app appear with every entry

--- server/server.js
+++ server/server.js
@@ -14,15 +14,12 @@ var io = socketIO(server);
 app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
-  console.log('new user connected');
+  console.log('New user connected');
 
-  socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
-    //io.emit sends to all connected users
-    socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
 
     socket.on('createMessage', (message, callback) => {
       console.log('createMessage', message);
